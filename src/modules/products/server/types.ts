1815424d0d41544edab6ca3ff05ce24962bda745
sunny <sunny@bytespark.app>
2025-04-23 17:19:45 +0800

    fix: remove useless types

--- src/modules/products/server/types.ts
+++ src/modules/products/server/types.ts
@@ -2,5 +2,5 @@ import { inferRouterOutputs } from "@trpc/server";
 
 import type { AppRouter } from "@/trpc/routers/_app";
 
-export type ProductsGetManyOutput = inferRouterOutputs<AppRouter>["products"]["getMany"];
-export type ProductsGetManyOutputSingle = ProductsGetManyOutput[number];
+export type ProductsGetManyOutput =
+  inferRouterOutputs<AppRouter>["products"]["getMany"];
